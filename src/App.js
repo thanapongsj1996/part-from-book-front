85f35f899a4f9f911c0c9c899009f26f9db46007
Boy Thanapong <thanapongsj1996@gmail.com>
2020-07-27 16:09:56 +0700

    Edited description & logo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          A Part From The Book v3
+          A Part From The Book
         </p>
       </header>
     </div>
