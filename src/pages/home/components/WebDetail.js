e7db06d748ca8ca86e5ba3dce7f08038538512bc
thanapongsj1996 <thanapongsj1996@gmail.com>
2020-08-25 01:46:14 +0700

    Fix web detail image size

--- src/pages/home/components/WebDetail.js
+++ src/pages/home/components/WebDetail.js
@@ -18,8 +18,8 @@ const useStyles = makeStyles((theme) => ({
     height: 4,
   },
   imgDetail: {
-    width: 150,
-    height: 150,
+    width: 100,
+    height: 100,
   },
   detailHeader: {
     textAlign: 'center',
