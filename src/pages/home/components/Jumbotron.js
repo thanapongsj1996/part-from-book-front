324b8101a91da03ee5985f759627294c3d18f6ca
Loklek <potsawat_3232@hotmail.com>
2021-01-05 21:01:26 +0700

    Home: update jumbotron background

--- src/pages/home/components/Jumbotron.js
+++ src/pages/home/components/Jumbotron.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { Grid, Container } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
-const bgImage =
-  'https://images.unsplash.com/photo-1472173148041-00294f0814a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+import background from 'assets/images/jumbotron-background.png'
 
 const useStyles = makeStyles((theme) => ({
   jumbotron: {
     width: '100%',
-    background: `linear-gradient( rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.3) ), url('${bgImage}')`,
+    background: `url(${background})`,
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
