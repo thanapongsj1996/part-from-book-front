6d79f98ec57e3d9e7efd89a68636b3fa05888fbd
Loklek <potsawat_3232@hotmail.com>
2021-01-09 18:59:19 +0700

    Header: split code & support toggle dark mode

---
+++ src/layouts/Header/components/HeaderNavbar.js
@@ -0,0 +1,126 @@
+import React, { useCallback } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+// import { Menu } from '@material-ui/icons'
+
+// import HeaderPopMenu from './HeaderPopmenu'
+import { NAV_LIST } from '../constants/header-nav-list.const'
+import COLOR from 'assets/scss/variables/__colors.scss'
+
+const useStyles = makeStyles((theme) => ({
+  navButton: {
+    fontSize: 18,
+    fontWeight: 400,
+    margin: theme.spacing(0, 1),
+  },
+  nav: {
+    color: COLOR.black,
+  },
+  navDark: {
+    color: COLOR.grey3,
+  },
+  navActive: {
+    color: COLOR.primary,
+    fontWeight: 600,
+  },
+  navDarkActive: {
+    color: COLOR.white,
+    fontWeight: 600,
+  },
+  authLabel: {
+    textTransform: 'none',
+  },
+  icon: {
+    color: COLOR.primary,
+  },
+  iconDark: {
+    color: COLOR.white,
+  },
+}))
+
+const HeaderNavbar = ({ darkMode, location }) => {
+  const classes = useStyles()
+  // const theme = useTheme()
+  // const isMobile = useMediaQuery(theme.breakpoints.down('xs'))
+
+  // const [profilePopover, setProfilePopover] = useState(null)
+
+  // useEffect(() => {
+  //   setProfilePopover(null)
+  // }, [isMobile])
+
+  // const onShowPopMenu = (event) => setProfilePopover(event.currentTarget)
+
+  const getNavClass = useCallback(
+    (url) => {
+      const classList = [classes.navButton]
+
+      if (darkMode) {
+        classList.push(classes.navDark)
+        if (location.pathname === url) {
+          classList.push(classes.navDarkActive)
+        }
+      } else {
+        classList.push(classes.nav)
+        if (location.pathname === url) {
+          classList.push(classes.navActive)
+        }
+      }
+
+      return classList.join(' ')
+    },
+    [
+      classes.nav,
+      classes.navActive,
+      classes.navButton,
+      classes.navDark,
+      classes.navDarkActive,
+      darkMode,
+      location.pathname,
+    ]
+  )
+
+  return (
+    <>
+      {/* <Hidden only={['xs']}> */}
+      {NAV_LIST.map((menu) => (
+        <Button
+          key={menu.title}
+          component={RouterLink}
+          to={menu.url}
+          className={getNavClass(menu.url)}
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+        >
+          {menu.title}
+        </Button>
+      ))}
+
+      {/* <Button
+              classes={{ root: classes.authLabel }}
+              className={classes.navButton}
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+            >
+              เข้าสู่ระบบ
+            </Button> */}
+      {/* </Hidden> */}
+
+      {/* <IconButton onClick={onShowPopMenu}>
+        <Menu
+          className={darkMode ? classes.iconDark : classes.icon}
+          fontSize="large"
+        />
+      </IconButton>
+
+      <HeaderPopMenu
+        profilePopover={profilePopover}
+        setProfilePopover={setProfilePopover}
+      /> */}
+    </>
+  )
+}
+
+export default withRouter(HeaderNavbar)
--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,266 +1,54 @@
-import React, { useState } from 'react'
-import { Link as RouterLink, useHistory, useRouteMatch } from 'react-router-dom'
-import {
-  AppBar,
-  Toolbar,
-  Link,
-  IconButton,
-  Divider,
-  Button,
-  Popover,
-  Hidden,
-  Grid,
-  Backdrop,
-  Container,
-} from '@material-ui/core'
-import { AccountCircleSharp, Menu } from '@material-ui/icons'
+import React from 'react'
 import { connect } from 'react-redux'
+import { AppBar, Toolbar, Container, Switch, Tooltip } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-import useStyles from './header-style'
-import logo from 'assets/images/book-logo.png'
+import HeaderLogo from './components/HeaderLogo'
+import HeaderNavbar from './components/HeaderNavbar'
 import { toggleDarkMode } from 'actions/app.action'
 
-const menuList = [
-  {
-    title: 'หน้าหลัก',
-    url: '/',
-  },
-  {
-    title: 'บทความ',
-    url: '/articles',
-  },
-  {
-    title: 'เกี่ยวกับเรา',
-    url: '',
-  },
-]
+import COLOR from 'assets/scss/variables/__colors.scss'
 
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer,
+    backgroundColor: COLOR.white,
+  },
+  appBarDark: {
+    zIndex: theme.zIndex.drawer,
+    backgroundColor: COLOR.dark1,
+    border: 0,
+  },
+  spacer: {
+    flexGrow: 1,
+  },
+}))
 const Header = ({ darkMode, actions, ...props }) => {
   const classes = useStyles()
-  const history = useHistory()
-  const { path } = useRouteMatch()
-
-  const [profilePopover, setProfilePopover] = useState(null)
-  const [menuPopover, setMenuPopover] = useState(null)
-  const [scrollTop, setScrollTop] = useState(0)
-
-  const onShowProfilePopover = (event) => {
-    setProfilePopover(event.currentTarget)
-  }
-  const onCloseProfilePopover = () => {
-    setProfilePopover(null)
-  }
-
-  const onShowMenuPopover = (event) => {
-    setMenuPopover(event.currentTarget)
-  }
-
-  const onCloseMenuPopover = () => {
-    setMenuPopover(null)
-  }
-
-  const showMenuPopover = Boolean(menuPopover)
-  const showProfilePopover = Boolean(profilePopover)
-
-  const navigateToProfile = () => history.push(`/article/1`)
-
-  window.onscroll = () => {
-    setScrollTop(document.documentElement.scrollTop)
-  }
 
   return (
     <AppBar
-      className={
-        scrollTop < 500 && path === '/' ? classes.appBarBlack : classes.appBar
-      }
+      className={darkMode ? classes.appBarDark : classes.appBar}
       position="fixed"
     >
       <Container>
         <Toolbar>
-          <Link
-            component={RouterLink}
-            to="/"
-            color="inherit"
-            underline="none"
-            className={classes.logoLink}
-          >
-            <img className={classes.logoImage} src={logo} alt="small-logo" />
-          </Link>
+          <HeaderLogo darkMode={darkMode} />
 
           <div className={classes.spacer} />
 
-          <Hidden only={['xs']}>
-            {menuList.map((menu) => (
-              <Button
-                key={menu.title}
-                component={RouterLink}
-                to={menu.url}
-                className={classes.navButton}
-                aria-controls="simple-menu"
-                aria-haspopup="true"
-              >
-                {menu.title}
-              </Button>
-            ))}
+          {/* dark mode swith */}
+          <Tooltip title="Toggle light/dark theme" arrow>
+            <Switch
+              checked={darkMode}
+              onChange={actions.toggleDarkMode}
+              color="secondary"
+              name="checkedB"
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+          </Tooltip>
 
-            {/* <Button
-              classes={{ root: classes.authLabel }}
-              className={classes.navButton}
-              aria-controls="simple-menu"
-              aria-haspopup="true"
-            >
-              เข้าสู่ระบบ
-            </Button> */}
-
-            <IconButton onClick={onShowProfilePopover}>
-              <AccountCircleSharp
-                fontSize="large"
-                classes={{
-                  root: classes.whiteIcon,
-                }}
-              />
-            </IconButton>
-
-            <Popover
-              id="popover-menu"
-              open={showProfilePopover}
-              anchorEl={profilePopover}
-              onClose={onCloseProfilePopover}
-              classes={{ paper: classes.popoverProfile }}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-            >
-              <Grid container>
-                <Button
-                  item
-                  xs={12}
-                  className={classes.navButtonPopoverProfile}
-                  aria-controls="simple-menu"
-                  aria-haspopup="true"
-                >
-                  <Grid
-                    container
-                    direction="row"
-                    justify="flex-start"
-                    alignItems="center"
-                    spacing={1}
-                  >
-                    <Grid item>
-                      <AccountCircleSharp
-                        className={classes.userIconPopoverProfile}
-                      />
-                    </Grid>
-
-                    <Grid item className={classes.profileSection}>
-                      <div className={classes.profileHeader}>
-                        Thanapong Somjai
-                      </div>
-                      <div className={classes.lookProfile}>ดูโปรไฟล์ของคุณ</div>
-                    </Grid>
-                  </Grid>
-                </Button>
-                <Grid item xs={12} className={classes.dividerPopover}>
-                  <Divider variant="middle" color="primary" />
-                </Grid>
-              </Grid>
-
-              <Grid container justify="center">
-                <Button item xs={12} className={classes.navButtonPopover}>
-                  ออกจากระบบ
-                </Button>
-              </Grid>
-            </Popover>
-          </Hidden>
-
-          <Hidden only={['sm', 'md', 'lg', 'xl']}>
-            <IconButton onClick={onShowMenuPopover}>
-              <Menu fontSize="large" className={classes.whiteIcon} />
-            </IconButton>
-
-            <Backdrop
-              className={classes.backdrop}
-              open={showMenuPopover}
-              onClick={onCloseMenuPopover}
-            >
-              <Popover
-                id="popover-menu"
-                open={showMenuPopover}
-                anchorEl={menuPopover}
-                onClose={onCloseMenuPopover}
-                classes={{ paper: classes.popover }}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'center',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'center',
-                }}
-              >
-                <Grid className={classes.typography} container>
-                  <Grid item xs={12} className={classes.textCenter}>
-                    <IconButton onClick={navigateToProfile}>
-                      <AccountCircleSharp className={classes.userIconPopover} />
-                    </IconButton>
-                  </Grid>
-
-                  <Grid item xs={12} className={classes.userNamePopover}>
-                    <Button
-                      className={classes.navButtonPopover}
-                      aria-controls="simple-menu"
-                      aria-haspopup="true"
-                      onClick={navigateToProfile}
-                    >
-                      Thanapong Somjai
-                    </Button>
-                  </Grid>
-
-                  <Grid item xs={12} className={classes.dividerPopover}>
-                    <Divider variant="middle" color="primary" />
-                  </Grid>
-
-                  {menuList.map((menu) => (
-                    <Grid
-                      item
-                      xs={12}
-                      key={menu.title}
-                      className={classes.textCenter}
-                    >
-                      <Button
-                        className={classes.navButtonPopover}
-                        aria-controls="simple-menu"
-                        aria-haspopup="true"
-                        component={RouterLink}
-                        to={menu.url}
-                      >
-                        {menu.title}
-                      </Button>
-                    </Grid>
-                  ))}
-
-                  <Grid item xs={12} className={classes.dividerPopover}>
-                    <Divider variant="middle" color="primary" />
-                  </Grid>
-
-                  <Grid item xs={12} className={classes.textCenter}>
-                    <Button
-                      className={classes.navButtonPopover}
-                      aria-controls="simple-menu"
-                      aria-haspopup="true"
-                    >
-                      ออกจากระบบ
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Popover>
-            </Backdrop>
-          </Hidden>
+          <HeaderNavbar darkMode={darkMode} />
         </Toolbar>
       </Container>
     </AppBar>
