0476b3a7b21d6573b52694d20bdffd0713d1196e
Loklek <potsawat_3232@hotmail.com>
2021-01-20 17:57:26 +0700

    Utils: add axios instant & response handle

---
+++ src/utils/httpClient.util.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const httpClient = axios.create({
+  timeout: 10000, //10 seconds
+})
+
+httpClient.interceptors.request.use(async (config) => {
+  return config
+})
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(error)
+    const { response } = error
+    return Promise.reject(response)
+  }
+)
+
+export default httpClient
