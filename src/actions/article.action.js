0c039c7ebcb524ea9490e73a16dc115068396544
thanapongsj1996 <thanapongsj1996@gmail.com>
2021-01-21 16:38:27 +0700

    Updated path, User avatar

--- src/actions/article.action.js
+++ src/actions/article.action.js
@@ -5,7 +5,7 @@ import { responseHandle } from 'utils/responseHandle.util'
 
 // Functions
 export const fetchArticles = (page = 1, limit = 5) => {
-  const url = `${API.getAllArticles()}/?page=${page}&limit=${limit}`
+  const url = `${API.getAllArticles()}?page=${page}&limit=${limit}`
   const request = httpClient.get(url).then((res) => res.data)
 
   return async (dispatch) => await responseHandle(dispatch, request)
