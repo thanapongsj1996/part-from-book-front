9948c6fd4265848eb4b4e2a95132830da220e204
Loklek <potsawat_3232@hotmail.com>
2021-01-30 19:03:21 +0700

    Constants: update yup validation

--- src/global/constants/yup.const.js
+++ src/global/constants/yup.const.js
@@ -1,27 +1,30 @@
 import * as yup from 'yup'
 
 export const YUP_VALIDATION = {
-  FIRST_NAME: yup
-    .string('Enter your first name')
-    .required('First name is required'),
-  LAST_NAME: yup
-    .string('Enter your last name')
-    .required('Last name is required'),
+  FIRST_NAME: yup.string().required('กรุณาระบุชื่อของคุณ'),
+  LAST_NAME: yup.string().required('กรุณาระบุนามสกุลของคุณ'),
   EMAIL: yup
-    .string('Enter your email')
-    .required('Email is required')
-    .email('Enter a valid email'),
+    .string()
+    .required('กรุณาระบุอีเมลของคุณ')
+    .email('อีเมลของคุณไม่ถูกต้อง'),
   PHONE_NUMBER: yup
-    .string('Enter your phone')
+    .string()
     .required('Phone number is required')
     .matches(/^(?:\d){3}-?(?:\d){3}-?(?:\d){4}$/, 'Enter a valid phone number'),
-  USERNAME: yup.string('Enter your username').required('Username is required'),
+  USERNAME: yup.string().required('กรุณาระบุชื่อผู้ใช้ของคุณ'),
   PASSWORD: yup
-    .string('Enter your password')
-    .required('Password is required')
-    .min(8, 'Password should be of minimum 8 characters length')
+    .string()
+    .required('กรุณาระบุรหัสผ่านของคุณ')
+    .min(8, 'รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร')
     .matches(
-      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[\]:;<>,.?/~_+\-=|\\]).{8,}$/,
-      'Must contain at least one uppercase letter, one lowercase letter, one number and one special character'
+      // /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[\]:;<>,.?/~_+\-=|\\]).{8,}$/,
+      // 'Must contain at least one uppercase letter, one lowercase letter, one number and one special character'
+      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+      'รหัสผ่านต้องประกอบไปด้วย ตัวพิมพ์ใหญ่, ตัวพิมพ์เล็ก และตัวเลขอย่างน้อยอย่างละ 1 ตัว'
     ),
+  PASSWORD_CONFIRM: yup
+    .string()
+    .required('กรุณายืนยันรหัสผ่านของคุณ')
+    .oneOf([yup.ref('password')], 'รหัสผ่านไม่ตรงกัน'),
+  PEN_NAME: yup.string().required('กรุณาระบุนามปากกาของคุณ'),
 }
